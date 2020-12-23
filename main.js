054a981e25e31abcb2fd5cb87ad0d4d42959d0ae
Tomas Di Mauro <tomasdimauro1@gmail.com>
2020-12-23 17:15:40 -0300

    Se añadió la busqueda con el Buscar con Google

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-
 let inputHTML = document.getElementById('gl-inp');
 inputHTML.addEventListener('keydown', (e)=>{
     let input = document.getElementById('gl-inp').value;
     let key = e.key;
     if(key == "Enter" || key == 13 || key == "13"){
         window.location.href = "https://www.google.com/search?q=" + input;
-        
-        
     }
+});
+let botonBuscar = document.getElementById('buscarConGoogle');
+botonBuscar.addEventListener('click', ()=>{
+    let input = document.getElementById('gl-inp').value;
+    window.location.href = "https://www.google.com/search?q=" + input;
 });
